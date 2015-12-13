ddb8f104465548194774edce797384725a91cf06
Aleksandr Nikolaev <aleksandr2012nikolaev@gmail.com>
2015-12-13 21:00:04 +0300

    задание2

---
+++ Task2_2/Main.java
@@ -0,0 +1,34 @@
+public class Main{
+	
+	
+	public static void main(String args[]){
+	
+	Pacient pacient = new Pacient("m","Ivan","Ivanov",70 , 180);
+	
+	Pasport pasport = new Pasport("Grodno",15987);
+		
+	Registry registry = new Registry();
+
+	Doctors doctors = new Doctors("terapevt");
+
+	pacient.setPasport(pasport);
+	System.out.println(pacient.getPasport());
+
+	pacient.setMedcard(registry.getMedcard());
+	System.out.println(pacient.getMedcard());
+
+	doctors.setMedcard(registry.getMedcard());
+	System.out.println(doctors.getMedcard());
+	
+	System.out.println(registry.getMedcard());
+
+	registry.setDoctors(doctors);
+	System.out.println(registry.getDoctors());
+	
+	registry.setPacient(pacient);
+	System.out.println(registry.getPacient());
+
+		
+
+}
+}
